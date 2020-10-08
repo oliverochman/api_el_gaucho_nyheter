2e7373829f25826ecda7fa51f3898676adc12d9d
FacundoOs <osores.facundo@gmail.com>
2020-10-08 13:24:44 +0200

    add content attribute to the Article database table

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :article do
     title { "MyString" }
     lead { "MyText" }
+    content { "The American people have witnessed what is the greatest failure of any presidential administration in the history of our country" }
   end
 end
