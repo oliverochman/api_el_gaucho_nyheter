df0854bc6ffd66e4df1d41d92eab95a72e25a57b
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-10-13 15:30:54 +0200

    Removes quotation marks from the seeds file. Adds sad path for journalist can create articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ articles = Article.create(
   [
     {
       title: "Ny studie: Möjligt att utplåna fattigdomen och samtidigt rädda klimatet",
-      lead: "Den "omöjliga ekvationen" går att lösa.",
+      lead: "Den 'omöjliga ekvationen' går att lösa.",
       category: "news",
       content: "Det är fullt möjligt att rädda jordens klimat och utplåna fattigdomen på samma gång, hävdar brittiska forskare. Och detta utan att kasta oss tillbaka till medeltiden.",
     },
@@ -21,19 +21,19 @@ articles = Article.create(
       title: "Benjamin Ingrosso får matshow på TV4",
       lead: "TV4 knyter ännu en i klanen Wahlgren till sig.",
       category: "politics",
-      content: "Storasyster Bianca Ingrosso, 25, sitter i "Talang"-juryn sedan flera år. Mamma Pernilla Wahlgren, 52, har precis tillkännagetts som del i expertpanelen i TV4:s nya storsatsning "Masked singer" och efter inspelningen av "Så mycket bättre" i somras står det nu klart att även lillebror Benjamin Ingrosso, 23, kommer att dyka upp mer i TV4 framöver. Förutom "Så mycket bättre" kommer han också att få ett eget program.",
+      content: "Storasyster Bianca Ingrosso, 25, sitter i 'Talang'-juryn sedan flera år. Mamma Pernilla Wahlgren, 52, har precis tillkännagetts som del i expertpanelen i TV4:s nya storsatsning 'Masked singer' och efter inspelningen av 'Så mycket bättre' i somras står det nu klart att även lillebror Benjamin Ingrosso, 23, kommer att dyka upp mer i TV4 framöver. Förutom 'Så mycket bättre' kommer han också att få ett eget program.",
     },
     {
       title: "Benjamin Ingrosso får matshow på TV4",
       lead: "TV4 knyter ännu en i klanen Wahlgren till sig.",
       category: "sports",
-      content: "Storasyster Bianca Ingrosso, 25, sitter i "Talang"-juryn sedan flera år. Mamma Pernilla Wahlgren, 52, har precis tillkännagetts som del i expertpanelen i TV4:s nya storsatsning "Masked singer" och efter inspelningen av "Så mycket bättre" i somras står det nu klart att även lillebror Benjamin Ingrosso, 23, kommer att dyka upp mer i TV4 framöver. Förutom "Så mycket bättre" kommer han också att få ett eget program.",
+      content: "Storasyster Bianca Ingrosso, 25, sitter i 'Talang'-juryn sedan flera år. Mamma Pernilla Wahlgren, 52, har precis tillkännagetts som del i expertpanelen i TV4:s nya storsatsning 'Masked singer' och efter inspelningen av 'Så mycket bättre' i somras står det nu klart att även lillebror Benjamin Ingrosso, 23, kommer att dyka upp mer i TV4 framöver. Förutom 'Så mycket bättre' kommer han också att få ett eget program.",
     },
     {
       title: "Benjamin Ingrosso får matshow på TV4",
       lead: "TV4 knyter ännu en i klanen Wahlgren till sig.",
       category: "politics",
-      content: "Storasyster Bianca Ingrosso, 25, sitter i "Talang"-juryn sedan flera år. Mamma Pernilla Wahlgren, 52, har precis tillkännagetts som del i expertpanelen i TV4:s nya storsatsning "Masked singer" och efter inspelningen av "Så mycket bättre" i somras står det nu klart att även lillebror Benjamin Ingrosso, 23, kommer att dyka upp mer i TV4 framöver. Förutom "Så mycket bättre" kommer han också att få ett eget program.",
+      content: "Storasyster Bianca Ingrosso, 25, sitter i 'Talang'-juryn sedan flera år. Mamma Pernilla Wahlgren, 52, har precis tillkännagetts som del i expertpanelen i TV4:s nya storsatsning 'Masked singer' och efter inspelningen av 'Så mycket bättre' i somras står det nu klart att även lillebror Benjamin Ingrosso, 23, kommer att dyka upp mer i TV4 framöver. Förutom kommer han också att få ett eget program.",
     },
   ]
 )
--- spec/requests/api/v1/admin/journalist_can_create_articles_spec.rb
+++ spec/requests/api/v1/admin/journalist_can_create_articles_spec.rb
@@ -52,4 +52,28 @@ RSpec.describe "POST /v1/admin/articles", type: :request do
       expect(response_json["message"]).to eq "Content can't be blank"
     end
   end
+
+  describe "unauthorized user" do
+    let(:unauthorized_user) { create(:user, role: "registered") }
+    let(:unauthorized_user_credentials) { unauthorized_user.create_new_auth_token }   
+    let(:unauthorized_headers) { { HTTP_ACCEPT: "application/json" }.merge!(unauthorized_user_credentials) }
+
+    before do
+      post "/api/v1/admin/articles",
+           params: {
+             title: "Trump holds first public event since Covid diagnosis",
+             lead: "Donald Trump has delivered a speech in front of cheering supporters at the White House in his first public appearance since being hospitalised",
+             content: "The event was officially a 'peaceful protest', but looked, critics said, much like a Trump campaign rally.",
+             category: "news",
+           }, headers: unauthorized_headers
+    end
+
+    it "is expected to return 401 response status" do
+      expect(response).to have_http_status 401
+    end
+
+    it "is expected to return error message" do
+      expect(response_json["message"].first).to eq "You need to sign in or sign up before continuing."
+    end
+  end
 end
