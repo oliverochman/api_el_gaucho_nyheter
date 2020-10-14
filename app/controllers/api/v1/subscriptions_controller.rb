e874daecee2e056481b4cce33cbccddb070e5dc8
oliverochman <oliver.ochman@hotmail.com>
2020-10-14 13:47:02 +0200

    add payment flow with stripe in subscriptions create

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,5 +1,33 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    binding.pry
+    begin
+      customer_id = get_customer(params[:stripeToken])
+      subscription = Stripe::Subscription.create({ customer: customer_id, plan: "el_g_subscription" })
+
+      status = Stripe::Invoice.retrieve(subscription.latest_invoice).paid
+
+      if status === true 
+        current_user.update_attribute(:role, 'subscriber')
+        render json: { message: "Subscription successfully created!"}, status: :created
+      else
+        render_stripe_error('You got no money!')
+      end
+    rescue => error
+      binding.pry
+    end
+  end
+
+  private
+
+  def get_customer(stripe_token)
+    customer = Stripe::Customer.list(email: current_user.email).data.first
+    customer ||= Stripe::Customer.create({ email: current_user.email, source: stripe_token })
+    customer.id
+  end
+
+  def render_stripe_error(error)
+    render json: { message: "Transaction was not successfull. #{error}"}, status: 422
   end
 end
