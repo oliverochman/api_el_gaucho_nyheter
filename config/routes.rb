2f221586a435df31fa3263bbb9004f0930801596
oliverochman <oliver.ochman@hotmail.com>
2020-10-14 13:29:39 +0200

    configure routes and setup subscriptions controller with empty create action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :articles, only: [:index, :show, :create]
+      resources :subscriptions, only: [:create]
       namespace :admin do
         resources :articles, only: [:create]
       end
